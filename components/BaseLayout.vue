8e3ab452326a5b1e92d618674a447da962bf8707
lmy <lmy@lmydeMac-Pro.lan>
2021-03-20 19:09:19 +0800

    init

---
+++ components/BaseLayout.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<view class="content">
+			<slot></slot>
+		</view>
+		<u-tabbar :list="tabbar" :mid-button="true"></u-tabbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "BaseLayout",
+		data() {
+			return {
+				tabbar: [{
+						iconPath: "/static/uview/example/component.png",
+						selectedIconPath: "/static/uview/example/component_select.png",
+						text: '组件',
+						count: 2,
+						isDot: true,
+						pagePath: "/pages/index/index"
+					},
+					{
+						iconPath: "/static/uview/example/js.png",
+						selectedIconPath: "/static/uview/example/js_select.png",
+						text: '工具',
+						midButton: true,
+						pagePath: "/pages/js/index"
+					},
+					{
+						iconPath: "/static/uview/example/template.png",
+						selectedIconPath: "/static/uview/example/template_select.png",
+						text: '模板',
+						pagePath: "/pages/template/index"
+					},
+				]
+			};
+		},
+		onLoad() {
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ pages/js/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<BaseLayout>
+		<image class="logo" src="/static/uview/common/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</BaseLayout>
+</template>
+
+<script>
+	import BaseLayout from "../../components/BaseLayout.vue"
+	export default {
+		components: {
+			BaseLayout
+		},
+		data() {
+			return {
+				title: '工具',
+				tabbar: ''
+			}
+		},
+		onLoad() {
+			
+		}
+	}
+</script>
+
+<style>
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
